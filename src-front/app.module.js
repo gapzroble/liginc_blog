54e34482513cc281d89e97cef0331a03ee4f0219
Randolph Roble <roblerm@gmail.com>
2018-10-25 22:26:07 +0800

    front specs

--- src-front/app.module.js
+++ src-front/app.module.js
@@ -8,3 +8,19 @@ app.value('Settings', {
 app.config(['$urlRouterProvider', function($urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
 }]);
+
+app.controller('app.controller', ['$scope', function($scope) {
+    $scope.closeMenu = function () {
+        var hamburger = $('.js-hamburger');
+        if (hamburger.hasClass('is-active')) {
+            hamburger.trigger('click');
+        }
+    }
+}]);
+
+app.run(['$transitions', function($transitions) {
+    $transitions.onSuccess({}, function(trans) {
+        // scroll to top
+        $('a.js-scroll').trigger('click');
+    });
+}]);
